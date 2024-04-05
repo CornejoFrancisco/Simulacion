32feddce5263239c77bc78322ff0d0613f012de4
Francisco Cornejo <109431998+CornejoFrancisco@users.noreply.github.com>
2024-04-05 19:46:27 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import math
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-from Soporte import*
+from soporte import*
 
 def menu():
     print("1 - Ingrese la cantidad de numero de datos deseado ")
@@ -15,44 +15,51 @@ def menu():
     print("Cualquier otro numero es la salida")
 
 
-
-
-
 def principal(param):
     cargado_numero = False
+
     op = -1
     while op != 4:
         menu()
         op = int(input("Ingrese el numero de la opcion deseado: "))
         if op == 1:
-            cantidad = validad_positivo()
+            cantidad = validad_positivo("Ingrese el numero de cantidad de datos deseado: ")
             vector = vector_cargado(cantidad)
-            mayor = max(vector)
-            minimo = min(vector)
 
-            rango = mayor-minimo
-            intervalos = math.sqrt(cantidad)
-            amplitud = rango/intervalos
-            media = media_datos(vector)
-            varianza = varianza_datos(vector)
             cargado_numero = True
+            print(vector)
+            uniforme_calculadora(vector, 0, 0, 10)
 
-        if op == 2:
+
+        elif op == 2:
             if cargado_numero:
-                punto_2(vector)
+                # Selecion del caso que sea uniforme
+                variable_a = int(input("Ingrese la variable A: "))
+                variable_b = int(input("Ingrese la variable B: "))
+
+                punto_2(vector, variable_a, variable_b)
+
             else:
                 print("No se cargo todavia el vector")
-        if op == 3:
+        elif op == 3:
             if cargado_numero:
-                punto_3(vector)
+                print("1 - lambda")
+                print("2 - Media")
+                opcion = validad_positivo("Ingrese la opcion deseada: ")
+                if opcion == 1:
+                    valor_lambda = validad_positivo("Ingrese el valor de lambda: ")
+                    punto_3_media(vector, valor_lambda)
+                if opcion == 2:
+                    valor_lambda = validad_positivo("Ingrese el valor de media: ")
+                    punto_3_lambda(vector)
             else:
                 print("No se cargo todavia el vector")
-        if op == 4:
+        elif op == 4:
             if cargado_numero:
                 punto_4(vector)
             else:
                 print("No se cargo todavia el vector")
-        if( op != 1 or op != 2 or op != 3 or op != 4):
+        elif( op != 1 or op != 2 or op != 3 or op != 4):
             print("Se cierra el programa!! ")
             break
 
