e20d28213e0f146844132aad433f2fcdf7f74913
Juampi270302 <juampylambertucci2002@gmail.com>
2024-04-13 15:17:12 -0300

    frecuencias agrupadas arregladas

--- pruebas/prueba_frec_esperada.py
+++ pruebas/prueba_frec_esperada.py
@@ -4,70 +4,62 @@ vec_fe = [49.1918, 24.8965, 12.6004, 6.3772, 3.2276, 2.4852, 3.5485, 2.3361, 1.6
 vec_fo = [1, 6, 8, 1, 6, 9, 5, 1, 7, 6, 1, 3, 4]
 vec_fe_2 = [0.2144, 0.3254, 0.1563, 0.02, 15.5621, 36.1523, 5.3623, 3.2512, 0.2641, 0.2361, 0.0054]
 vec_fo_2 = [1, 6, 8, 1, 6, 9, 5, 1, 7, 6, 1]
-vec_fe_3 = [0.2144, 0.3254, 0.1563, 4.4125, 1, 15.5621, 36.1523, 5.3623, 3.2512, 0.2641, 2.25, 4.88, 1, 4.9999, 0.025]
-vec_fo_3 = [1, 6, 8, 1, 6, 9, 5, 1, 7, 6, 1, 3, 3, 6, 7]
+#vec_fe_3 = [0.2144, 0.3254, 0.1563, 4.4125, 1, 15.5621, 36.1523, 5.3623, 3.2512]
+#vec_fo_3 = [1, 6, 8, 1, 6, 9, 5, 1, 7]
+vec_fe_3 = [0.2144, 0.3254, 0.1563, 4.4125, 1, 15.5621, 36.1523, 5.3623, 3.2512, 0.2641, 2.25, 4.88, 1, 4.9999, 0.025, 0.002, 0.001]
+vec_fo_3 = [1, 6, 8, 1, 6, 9, 5, 1, 7, 6, 1, 3, 3, 6, 8, 7, 6]
 
-def frec_esp(vec_f, vec_o):
+
+def frec_esp2(vec_f, vec_o):
     vec_agrupados = []
     vec_oac = []
-    vec_ind_ag = []
     vec_li = []
     vec_ls = []
-    indice_li = 0
-    indice_ls = 0
     suma = 0
     acum = 0
 
     primero = True
-    for x in vec_f:
-        ind_x = vec_f.index(x)
+    for i in range(len(vec_f)):
+        x = vec_f[i]
         if x < 5:
             suma += x
-            acum += vec_o[ind_x]
+            acum += vec_o[i]
 
             if primero:
-                indice_li = ind_x
                 primero = False
-                vec_li.append(ind_x)
-                print("li zona primero:", indice_li)
+                vec_li.append(i)
 
             if suma >= 5:
                 vec_agrupados.append(round(suma, 4))
                 vec_oac.append(round(acum, 4))
                 suma = 0
                 acum = 0
-
-                indice_ls = ind_x
-                ind_x_1 = vec_f.index(x)
-                print(ind_x_1)
-                vec_ls.append(ind_x)
-                print("ls zona suma >= 5:", indice_ls)
-
+                vec_ls.append(i)
                 primero = True
 
-            if vec_f.index(x) == len(vec_f) - 1 and suma < 5:
+            elif i == len(vec_f) - 1 and suma < 5:
                 vec_agrupados[-1] += round(suma, 4)
                 vec_oac[-1] += round(acum, 4)
+
+                if suma != x:
+                    vec_ls[-1] = i
+                    del vec_li[-1]
+                elif suma == x:
+                    vec_ls.append(i)
+                    vec_li[-1] = vec_li[-1] - 1
+
                 suma = 0
                 acum = 0
-
-                indice_ls = ind_x
-                print("ls zona ultimo elemento:", indice_ls)
-
-                primero = True
 
         else:
             if 0 < suma < 5:
                 vec_agrupados.append(round(x + suma, 4))
-                vec_oac.append(round(acum + vec_o[ind_x], 4))
-
-                indice_ls = ind_x
-                vec_ls.append(ind_x)
-                print("ls zona suma < 5 y estas en un > 5:", indice_ls)
+                vec_oac.append(round(acum + vec_o[i], 4))
+                vec_ls.append(i)
 
             else:
                 vec_agrupados.append(x)
-                vec_oac.append(vec_o[ind_x])
+                vec_oac.append(vec_o[i])
             primero = True
             suma = 0
             acum = 0
@@ -82,4 +74,55 @@ def frec_esp(vec_f, vec_o):
     print(vec_ls)
 
 
+def frec_esp(vec_f, vec_o):
+    vec_agrupados = []
+    vec_oac = []
+    vec_li = []
+    vec_ls = []
+    vec_intervalos = []
+    suma = 0
+    acum = 0
+    primero = True
+
+    for i in range(len(vec_f)):
+        x = vec_f[i]
+        suma += x
+        acum += vec_o[i]
+        if primero:
+            vec_li.append(i)
+            primero = False
+
+        if suma >= 5:
+            vec_agrupados.append(round(suma, 4))
+            vec_oac.append(acum)
+            suma = 0
+            acum = 0
+            vec_ls.append(i)
+            primero = True
+        elif 0 < suma < 5 and i == len(vec_f) - 1:
+            vec_agrupados[-1] += round(suma, 4)
+            vec_oac[-1] += acum
+            del vec_li[-1]
+            vec_ls[-1] = i
+
+    for i in range(len(vec_ls)):
+        inicial = vec_li[i]
+        final = vec_ls[i]
+        if inicial != final:
+            intervalo = f"{inicial} - {final}"
+            vec_intervalos.append(intervalo)
+        elif inicial == final:
+            intervalo = f"{inicial}"
+            vec_intervalos.append(intervalo)
+
+    vector_relleno = ["" for i in range(len(vec_f) - len(vec_agrupados))]
+    vec_agrupados += vector_relleno
+    vec_oac += vector_relleno
+    vec_intervalos += vector_relleno
+
+    df = pd.DataFrame({"Vector_fo": vec_o, "Vector_fe": vec_f,"intervalos agrupados": vec_intervalos,
+                       "vector_fo_ag": vec_oac, "vector_fe_ag": vec_agrupados})
+    print(df)
+
 frec_esp(vec_fe_3, vec_fo_3)
+#frec_esp2(vec_fe_2, vec_fo_2)
